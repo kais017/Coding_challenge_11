76ad8136575e5a10d54a02d8457c549bf0f169e5
Your Name <your.email@example.com>
2025-02-25 15:58:32 -0500

    Task 1 - Created Book Class and fixed typos

--- cc_11.js
+++ cc_11.js
@@ -2,7 +2,7 @@
 
 class Book {
 constructor(title, author, isbn, copies) {
-this.tile = title
+this.title = title
 this.author = author
 this.isbn = isbn
 this.copies = copies
@@ -14,12 +14,12 @@ return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies
 } // method that returns formattes string of book details
 
 updateCopies(quantity) { 
-    this.copeis += quanitity;
+    this.copies += quantity;
 // method that updates the amount of available copies when a book is borrowed or returned
   }
 }
 
-// test cases: 
+//Test cases: 
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails()); // adding a new book and returning details
 
