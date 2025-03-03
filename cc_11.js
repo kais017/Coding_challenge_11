59c465f67c8abf5f154817ff592f6397c4996229
Your Name <your.email@example.com>
2025-03-02 19:50:30 -0500

    Task 2 - Created Borrower Class and added comment to task 1

--- cc_11.js
+++ cc_11.js
@@ -20,11 +20,41 @@ updateCopies(quantity) {
 }
 
 //Test cases: 
+console.log("Library Books:"); // adding a title to task 1 on console
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails()); // adding a new book and returning details
 
 book1.updateCopies(-1); // updating the quantity 
 console.log(book1.getDetails()); // logging the test case details
 
+//Task 2: Creating a Borrower Class
+
+class Borrower {
+constructor(name, borrowerId, borrowedBooks) {
+    this.name = name
+    this.borrowerId = borrowerId
+    this.borrowedBooks = []; // creating an array of borrowed books
+} // creating a class of borrowers
+
+borrowBook(book) {  //method to add book title to borrowedBooks
+    this.borrowedBooks.push(book); // adds book to end of array
+
+}
+ returnBook(book){ // method to remove book from array
+    const index = this.borrowedBooks.indexOf(book);
+    if (index > -1) { 
+        this.borrowedBooks.splice(index, 1); //removes book from array
+    }
+ }
+}
+
+//test cases:
+console.log("Books Borrowed:"); // adding a title to task 2 on console
+const borrower1 = new Borrower("Alice Johnson", 201); // creating an object to identify the borrower
+borrower1.borrowBook("The Great Gatsby"); // outputs the book borrowed 
+console.log(borrower1.borrowedBooks); // logs the book 
+
+borrower1.returnBook("The Great Gatsby"); // returns the book
+console.log(borrower1.borrowedBooks);
 
 
